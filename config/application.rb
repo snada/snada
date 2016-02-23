c7f05fb1005674f654c629dca382e18d1d3d178f
snada <stefano.nada@gmail.com>
2016-02-23 11:40:02 +0100

    [2] App bowerized and bootstrapped fix #2

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module Snada
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+
     Dotenv.load
   end
 end
